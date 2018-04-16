b1df556984a0d35060e326fbc0b9a740bc7008d6
Sarin Na Wangkanai <sarin@wangkanai.com>
2018-04-16 23:07:26 +0700

    thinking to regex per browser tag

---
+++ src/Wangkanai.Detection.Browser/Regexs/AgentRegex.cs
@@ -0,0 +1,15 @@
+﻿namespace Wangkanai.Detection.Regexs
+{
+    internal class AgentRegex
+    {
+        internal bool Match { get; private set; }
+        protected readonly string agent;
+
+        public AgentRegex(string agent)
+        {
+            this.agent = agent;
+        }
+
+        protected
+    }
+}
---
+++ src/Wangkanai.Detection.Browser/Regexs/MsieRegex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wangkanai.Detection.Regexs
+{
+    internal class MsieRegex : AgentRegex
+    {
+        public MsieRegex(string agent) : base(agent) { }
+    }
+
+    internal class Ie11Regex : AgentRegex
+    {
+        public Ie11Regex(string agent) : base(agent) { }
+    }
+
+    internal class OperaRegex : AgentRegex
+    {
+        public OperaRegex(string agent) : base(agent) { }
+    }
+
+    internal class EdgeRegex : AgentRegex
+    {
+        public EdgeRegex(string agent) : base(agent) { }
+    }
+    internal class Safari : AgentRegex
+    {
+        public Safari(string agent) : base(agent) { }
+    }
+
+    internal class Chrome : AgentRegex
+    {
+        public Chrome(string agent) : base(agent) { }
+    }
+}
